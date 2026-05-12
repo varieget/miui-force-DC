20bf2c60cd5f4ed3eb4a0fe602c0410787972c29
EN <varieget@gmail.com>
2026-05-12 13:33:02 +0800

    fix: SELinux context of vendor_file (#7)
    Fixes #6

--- service.sh
+++ service.sh
@@ -2,9 +2,9 @@
 MODDIR=${0%/*}
 
 set_dc_backlight() {
-  DC_BACK_LIGHT=$(settings list system | grep dc_back_light)
+  DC_BACK_LIGHT=$(cmd settings get system dc_back_light 2>/dev/null)
 
-  if [[ $DC_BACK_LIGHT == *"dc_back_light=1"* ]]; then
+  if [ $DC_BACK_LIGHT == "1" ]; then
     resetprop 'persist.vendor.dc_backlight.enable' 'true'
     input keyevent 26
   fi
